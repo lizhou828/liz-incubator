154564eda2f4612b8908015b7c39dc84224f9cda
lizhou <lizhou@111.com.cn>
2016-09-23 11:51:47 +0800

    1、modified liz-rapid-framework module

--- liz-rapid-framework/template/java_src/${basepackage_dir}/service/${className}Service.java
+++ liz-rapid-framework/template/java_src/${basepackage_dir}/service/${className}Service.java
@@ -1,56 +1,37 @@
 <#include "/java_copyright.include">
-<#assign className = table.className>
-<#assign classNameLower = className?uncap_first>
+<#assign className=table.className>
+<#assign classNameLower=className?uncap_first>
 
 package ${basepackage}.service;
 
-import com.liz.common.pojo.Pagination;
-import ${basepackage}.mapper.${className}Mapper;
 import ${basepackage}.model.${className};
+import com.liz.common.pojo.Pagination;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional(rollbackFor = Exception.class)
-@Service
-public class ${className}Service {
-
-    private ${className}Mapper ${classNameLower}Mapper;
-
-    @Autowired
-    public void set${className}Mapper(${className}Mapper ${classNameLower}Mapper) {
-        this.${classNameLower}Mapper = ${classNameLower}Mapper;
-    }
+public interface ${className}Service{
 
     /**
      * 通过主键查询实体对象
+     *
      * @param primaryKey
      * @return
      * @throws Exception
      */
-    public ${className} getByPK(java.lang.Integer primaryKey) throws Exception {
-        return ${classNameLower}Mapper.getByPK(primaryKey);
-    }
+    public ${className} getByPK(java.lang.Integer primaryKey) throws Exception;
 
     /**
      * 查询所有记录
      * @return
      * @throws Exception
      */
-    public List<${className}> list() throws Exception {
-        return ${classNameLower}Mapper.list();
-    }
+    public List<${className}> list() throws Exception;
 
     /**
      * 根据查询条件查询所有记录
      * @return
      * @throws Exception
      */
-    public List<${className}> listByProperty(${className} ${classNameLower})throws Exception {
-        return ${classNameLower}Mapper.listByProperty(${classNameLower});
-    }
+    public List<${className}> listByProperty(${className} ${classNameLower}) throws Exception;
 
 
     /**
@@ -59,18 +40,14 @@ public class ${className}Service {
      * @return
      * @throws Exception
      */
-    public int deleteByPK(java.lang.Integer primaryKey) throws Exception {
-        return ${classNameLower}Mapper.deleteByPK(primaryKey);
-    }
+    public int deleteByPK(java.lang.Integer primaryKey) throws Exception;
 
     /**
      * 根据多个主键删除记录
      * @param primaryKeys
      * @throws Exception
      */
-    public void deleteByPKeys(List<java.lang.Integer> primaryKeys) throws Exception {
-        ${classNameLower}Mapper.deleteByPKeys(primaryKeys);
-    }
+    public void deleteByPKeys(List<java.lang.Integer>primaryKeys) throws Exception;
 
     /**
      * 根据传入参数删除记录
@@ -78,9 +55,7 @@ public class ${className}Service {
      * @return
      * @throws Exception
      */
-    public int deleteByProperty(${className} ${classNameLower}) throws Exception {
-        return ${classNameLower}Mapper.deleteByProperty(${classNameLower});
-    }
+    public int deleteByProperty(${className} ${classNameLower}) throws Exception;
 
     /**
      * 保存记录
@@ -88,9 +63,7 @@ public class ${className}Service {
      * @return
      * @throws Exception
      */
-    public void save(${className} ${classNameLower}) throws Exception {
-        ${classNameLower}Mapper.save(${classNameLower});
-    }
+    public void save(${className} ${classNameLower}) throws Exception;
 
     /**
      * 更新记录
@@ -98,9 +71,7 @@ public class ${className}Service {
      * @return
      * @throws Exception
      */
-    public int update(${className} ${classNameLower}) throws Exception{
-        return ${classNameLower}Mapper.update(${classNameLower});
-    }
+    public int update(${className} ${classNameLower}) throws Exception;
 
     /**
      * 根据条件查询记录条数
@@ -108,19 +79,13 @@ public class ${className}Service {
      * @return
      * @throws Exception
      */
-    public int findByCount(${className} ${classNameLower}) throws Exception {
-        return ${classNameLower}Mapper.findByCount(${classNameLower});
-    }
+    public int findByCount (${className} ${classNameLower}) throws Exception;
 
     /**
      * 根据查询条件查询分页记录
      * @return
      * @throws Exception
      */
-    public Pagination<${className}> findByPagination(Pagination<${className}> pagination, ${className} ${classNameLower}) throws Exception{
-        List<${className}> list = ${classNameLower}Mapper.findByPagination(pagination, ${classNameLower});
-        pagination.setResultList(list);
-        return pagination;
-    }
+    public Pagination<${className}> findByPagination(Pagination<${className}> pagination,${className} ${classNameLower}) throws Exception;
 
 }
