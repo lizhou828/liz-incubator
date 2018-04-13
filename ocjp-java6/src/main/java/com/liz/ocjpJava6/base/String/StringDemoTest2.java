a403b7dbd0c94bf6099715cdb479f449fe00e93e
lizhou828 <lizhou828@126.com>
2018-04-13 10:06:48 +0800

    1、线程 2、基础

---
+++ ocjp-java6/src/main/java/com/liz/ocjpJava6/base/String/StringDemoTest2.java
@@ -0,0 +1,26 @@
+/*
+ *Project: liz-incubator
+ *File: com.liz.ocjpJava6.base.String.StringDemoTest2.java <2018年04月12日}>
+ ****************************************************************
+ * 版权所有@2015 国裕网络科技  保留所有权利.
+ ***************************************************************/
+
+package com.liz.ocjpJava6.base.String;
+
+/**
+ * @author lizhou
+ * @version 1.0
+ * @Date 2018年04月12日 15时29分
+ */
+public class StringDemoTest2 {
+
+    public static void main(String[] args) {
+        String str ="1234";
+        changeStr(str);
+        System.out.println(str);
+    }
+
+    public static void changeStr(String str) {
+        str = "welcome";
+    }
+}
