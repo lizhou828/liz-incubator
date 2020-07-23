a2e00ffee267e6895b32c8a8e388b48d9c4e89da
lizhou828 <lizhou@glorypty.com>
2020-07-23 16:37:14 +0800

    JDK8总结： HashMap 和 currentHashMap

---
+++ ocjp-java/src/main/java/com/liz/jdk8/CurrentHashMapTest.java
@@ -0,0 +1,11 @@
+package com.liz.jdk8;
+
+/**
+ *
+ * HashMap 和 currentHashMap JDK8总结
+ * https://blog.csdn.net/weixin_43543184/article/details/103936389
+ * @author: Created by lizhou
+ * @create: 2020-07-23 16:10
+ **/
+public class CurrentHashMapTest {
+}
--- ocjp-java/src/main/java/com/liz/jdk8/闭包.java
+++ ocjp-java/src/main/java/com/liz/jdk8/闭包.java
@@ -3,6 +3,7 @@ package com.liz.jdk8;
 import java.util.stream.Stream;
 
 /**
+ * 闭包：把方法作为参数进行传递
  * @author: Created by lizhou
  * @create: 2020-07-23 15:33
  **/
